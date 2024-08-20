df2945ee8c6e4e3fc4719af6f452ee874b7ec2dd
Shanks0465 <umashanks99@gmail.com>
2024-08-20 20:00:28 +0530

    Replicated Index and Navigation

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./global.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ChakraProvider>{children}</ChakraProvider>
+        <ChakraProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ChakraProvider>
       </body>
     </html>
   );
