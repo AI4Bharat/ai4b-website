ad911ec9ac80dd141b29f51555cff2a5b5ad8997
Shanks0465 <umashanks99@gmail.com>
2024-08-26 02:14:03 +0530

    Base Try Out Slug Test

--- frontend/src/app/areas/model/[...slug]/page.tsx
+++ frontend/src/app/areas/model/[...slug]/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { useParams } from "next/navigation";
+
 export default function ModelView() {
-  return <div>Try Out Model</div>;
+  const params = useParams();
+  const slug = params.slug;
+  return <div>{slug}</div>;
 }
