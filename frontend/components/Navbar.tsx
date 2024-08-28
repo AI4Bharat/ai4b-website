bc9f7927108110663b965021ac1c62c3671b0053
Shanks0465 <umashanks99@gmail.com>
2024-08-28 22:06:14 +0530

    Nav Path Test

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -239,7 +239,7 @@ interface NavItem {
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-  { label: "Home", href: "/" },
+  { label: "Home", href: `${imagePrefix}` },
   {
     label: "Areas",
     children: [
