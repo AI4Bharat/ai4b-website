7b3db73ba126bf68e023e0f4cfbe5763e2cf0dce
imradhe <radhesyam.musicals@gmail.com>
2024-09-12 21:01:56 +0530

    implemented owl-carousel

--- frontend/components/NewsSection.tsx
+++ frontend/components/NewsSection.tsx
@@ -1,12 +1,25 @@
+"use client";
 import React from "react";
-import { Box, Flex, Text, Heading, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  VStack,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import AI4BContainer from "./AI4BContainer";
 import { useQuery } from "react-query";
 import { API_URL } from "@/app/config";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { imagePrefix } from "@/app/config";
-import Link from "next/link";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import $ from "jquery";
+import dynamic from "next/dynamic";
 
 const fetchNews = async () => {
   try {
@@ -28,6 +41,7 @@ const News = () => {
     }[]
   >([]);
   const { isLoading, error, data } = useQuery("fetchNews", fetchNews);
+  const [scrollIndex, setScrollIndex] = useState(0);
 
   useEffect(() => {
     if (error || isLoading) {
@@ -37,44 +51,85 @@ const News = () => {
     }
   }, [error, data, isLoading]);
 
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // Ensure jQuery is available before initializing Owl Carousel
+    if (typeof window !== "undefined") {
+      window.$ = window.jQuery = $;
+    }
+    setIsMounted(true);
+  }, []);
+
+  // Dynamically import OwlCarousel to prevent SSR
+  const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+    ssr: false,
+  });
+
+  const options = {
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 10,
+    autoplay: true,
+    dots: false,
+    autoplayTimeout: 1500,
+    smartSpeed: 800,
+    nav: true,
+    navText: ["<-", "->"],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  };
+
+  if (!isMounted) {
+    return null; // Render nothing until the component mounts (on the client side)
+  }
+
   return (
-    <VStack p={5}>
+    <AI4BContainer>
       <Heading
         fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
         fontWeight={"bold"}
+        textAlign={"center"}
+        padding={"20px 0"}
       >
         <Text textColor={"a4borange"}> News</Text>
       </Heading>
-      <Box overflowX="auto" p={4} maxWidth="100%">
-        <Flex gap={4}>
-          {news.map((card, index) => (
-            <Box
-              key={index}
-              w={300}
-              borderWidth="1px"
-              borderRadius="lg"
-              backgroundColor="white"
-              boxShadow="md"
-              flexShrink={0}
-            >
-              <Image
-                src={card.image}
-                alt="Profile Photo"
-                height={300}
-                width={300}
-              />
-              <Box p={4}>
-                <Text fontWeight="bold">{card.title}</Text>
-                <Text noOfLines={4}>{card.description}</Text>
-                <Link href={`${imagePrefix}/news/${card.id}`}>
-                  <Text textColor={"a4borange"}>Read More</Text>
-                </Link>
-              </Box>
+
+      <OwlCarousel className="owl-carousel owl-theme" {...options}>
+        {news.map((card, index) => (
+          <LinkBox
+            className="item"
+            key={index}
+            as="article"
+            w={300}
+            borderWidth="1px"
+            borderRadius="lg"
+            backgroundColor="white"
+            boxShadow="md"
+            flexShrink={0}
+          >
+            <Image src={card.image} alt="News Image" height={300} width={300} />
+            <Box p={4}>
+              <Text fontWeight="bold">{card.title}</Text>
+              <Text noOfLines={4}>{card.description}</Text>
+              <LinkOverlay href={`${imagePrefix}/news/${card.id}`}>
+                <Text textColor={"a4borange"}>Read More</Text>
+              </LinkOverlay>
             </Box>
-          ))}
-        </Flex>
-      </Box>
-    </VStack>
+          </LinkBox>
+        ))}
+      </OwlCarousel>
+    </AI4BContainer>
   );
 };
 
