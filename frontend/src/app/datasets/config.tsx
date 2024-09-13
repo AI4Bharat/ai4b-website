0a324a6afab18fe07c2eaee6acbd509f50e5554c
Shanks0465 <umashanks99@gmail.com>
2024-09-13 19:24:31 +0530

    Datasets

---
+++ frontend/src/app/datasets/config.tsx
@@ -0,0 +1,12 @@
+export const datasets = [
+  { title: "svarah" },
+  { title: "lahaja" },
+  { title: "shrutilipi" },
+  { title: "sangraha" },
+  { title: "samanantar" },
+  { title: "aksharantar" },
+  { title: "IndicVoices-R" },
+  { title: "IndicOOV" },
+  { title: "FBI" },
+  { title: "Bhasha-Abhijnaanam" },
+];
