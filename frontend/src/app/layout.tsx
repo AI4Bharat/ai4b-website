648deb832e9985aaa239e31ee6399fc8d3418d6e
imradhe <radhesyam.musicals@gmail.com>
2024-09-16 01:31:20 +0530

    Added icons and keywords

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
   title: "AI4Bharat",
   description:
     "AI4Bharat is a research lab at IIT Madras which works on developing open-source datasets, tools, models and applications for Indian languages.",
+  icons: ["/assets/logos/ai4blogolarge.png"],
+  keywords: ["AI4Bharat", "IIT Madras", "research", "open-source", "Indian languages", "AI", "natural language processing", "NLP", "speech recognition", "speech synthesis", "asr", "tts", "language models", "llm", "bhashini", "digital india", "computer vision", "machine learning", "deep learning"],
   openGraph: {
     images: "/assets/logos/ai4blogolarge.png",
   },
