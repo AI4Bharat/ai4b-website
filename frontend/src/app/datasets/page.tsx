53d8ef07127d620f0f1d0566956c82673c6d1967
imradhe <radhesyam.musicals@gmail.com>
2024-09-19 03:05:41 +0530

    seo

--- frontend/src/app/datasets/page.tsx
+++ frontend/src/app/datasets/page.tsx
@@ -1,4 +1,26 @@
+import { Metadata } from "next";
 import DatasetsList from "../../../components/DatasetsPage";
+
+
+const title = "Datasets";
+const description = "Explore AI4Bharat’s extensive collection of open-source datasets developed for Indian languages. From speech data to translation and linguistic resources, these datasets support a wide range of AI models, including IndicVoices, Shrutilipi, Samanantar, IndicCorp, and more.";
+const keywords = "AI4Bharat, datasets, IndicVoices, Shrutilipi, Samanantar, IndicCorp, open-source, machine learning, Indian languages, speech data, translation, linguistic resources";
+
+
+export const metadata: Metadata = {
+  title: `${title}`,
+  description: `${description}`,
+  keywords: `${keywords}`,
+  openGraph: {
+    title: `${title}`,
+    description: `${description}`,
+  },
+  twitter: {
+    description: `${description}`,
+    card: "summary",
+    creator: "@ai4bharat",
+  },
+};
 
 export default function DatasetsPage() {
   return <DatasetsList />;
