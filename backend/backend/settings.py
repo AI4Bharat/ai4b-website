1c30dfd66ec636f3631e84513a0359c28fb889ac
sagarika-padmanaban <sagarikapd@gmail.com>
2025-06-24 18:13:52 +0530

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="/ai4b-website/backend/.env")
+load_dotenv(dotenv_path="/home/azure/ai4b-website/backend/.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,13 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","admin.models.ai4bharat.org","ai4bharat.iitm.ac.in"]
 
 TIME_ZONE = "Asia/Kolkata"
 
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 
 
 # Application definition
