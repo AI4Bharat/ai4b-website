5faca91889f29d59eba328d5c216a38866606b12
srihari <sriharikrishna07@gmail.com>
2025-07-05 11:12:50 +0530

    added ocr option

--- backend/areas/models.py
+++ backend/areas/models.py
@@ -18,7 +18,7 @@ class Area(models.TextChoices):
     NLP = "NLP"
     NLG = "NLG"
     NLU = "NLU/PEFT"
-    OCR = "OCR"
+    OCR = "OCR" # added ocr option 
     
 class License(models.Model):
     id = models.AutoField(primary_key=True)
